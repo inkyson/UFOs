a03301ca1f3a0214cfda0d7e8947edd379995a42
Inky <67121386+inkyson@users.noreply.github.com>
2020-09-26 22:10:41 -0400

    11.5.4

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select('tbody')
 
-// Simple JavaScript console.log statement
-function printHello() {
-    console.log("Hello there!");
-}
+// // Simple JavaScript console.log statement
+// function printHello() {
+//     console.log("Hello there!");
+// }
 
 function buildTable(data) {
     // First, clear out any existing data
@@ -30,3 +30,39 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+    // select = select the very first element that matches our selector string #datetime
+    // selector string (datetime) is the item we're telling D3.js to look for
+    // .property("value") telling to actually grab information instead of just looking for it
+    // & hold it in the date variable
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the data using that table
+    if (date) {
+        // this says "show only the rows where the date is equal to the date filter we created above"
+        // === tests for equality, meaning that the date in the table has to match the filter exactly.
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will just be
+    //the original tableData.
+    buildTable(filteredData);
+};
+
+// if-statement syntax
+// if ( condition ) { code to execute }
+
+// // pseudocode practice
+// if (a date is entered) {
+//     Filter the default data to show only the date entered
+// };
+
+// link our code directly to the filter button.
+// execute handleClick function when the button with an id of filter-btn is clicked.
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// call the buildTable function using the original data
+buildTable(tableData);
